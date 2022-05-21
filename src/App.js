00eeb726163ce54153a1a831b414a5c82713730a
JpTanaka <jpao.com@gmail.com>
2022-05-21 13:37:07 +0200

    added movements and style

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function generaterandomnumber(matrix) {
     for (let j=0; j<matrix.length; j++) {
       for(let i = 0; i<matrix[j].length; i++) {
         if (matrix[j][i] == 0) {
-          //console.log(i, j);
           dict[counter] = [i,j];
           counter++;
         }
@@ -20,105 +19,194 @@ function generaterandomnumber(matrix) {
 
 function mergesquares(e, matrix) {
   let played = false;
-  //matrix = Array(4).fill().map(()=> Array(4).fill(0));
-  //matrix[0] = [0,2,0,0];
+  var copya = [];
+for (var i = 0; i < matrix.length; i++)
+    copya[i] = matrix[i].slice();
   switch (e.code) {
-    case "ArrowLeft":
+     case "ArrowLeft":
       for (let i=0; i<matrix.length; i++) {
-        let pointer = -1;
+        let pointer = 0;
+        let arr = Array(matrix[i].length).fill(0);
         for(let j=0; j<matrix[i].length; j++) {
-          //Pointer aponta um a menos que o primeiro número maior que zero.
-          
           if(matrix[i][j] != 0) {
-            console.log(i, j, matrix[i][j], pointer);
-            matrix[i][pointer+1] = matrix[i][j];
-            if(j!=(pointer+1)){
-              matrix[i][j] = 0;
-              played = true;
+            if (arr[pointer]==0) {
+              arr[pointer] = matrix[i][j];
             }
-            pointer++;
-            for(let k = pointer+1; k<matrix[i].length; k++) {
-              if (matrix[i][k] !=0) {
-                console.log(matrix[i], k, pointer);
-                if (matrix[i][k] == matrix[i][pointer]) {
-                  matrix[i][pointer] +=matrix[i][pointer];
-                  matrix[i][k] = 0;
-                  break;
-                }
-                console.log(matrix[i], k, pointer);
-                if((pointer+1) != k){
-                matrix[i][pointer+1] = matrix[i][k];
-                matrix[i][k]=0;
-                played = true;
-              }
-                console.log(matrix[i], k, pointer, "aaa");
-                break;
-              }
+            else if(matrix[i][j] == arr[pointer] && j!=pointer) {
+              arr[pointer]*=2;
+              matrix[i][j]=0;
+              pointer++;
             }
-            
+            else if(matrix[i][j] != arr[pointer] && arr[pointer] !=0) {
+                arr[pointer+1] = matrix[i][j];
+                matrix[i][j] = 0;
+                pointer++;
+            }
           }
           }
+          for(let j=0;j<matrix[i].length; j++) {
+            if(copya[i][j] != arr[j]) played=true; 
+          }
+          matrix[i]=arr;
         }
-      
-      console.log("esquerda");
       break;
     case "ArrowUp":
-      console.log("aa");
-      return;
+      //j is the collum of the matrix
+        for(let j=0; j<matrix[0].length; j++) {
+          let pointer = 0;
+          let arr = Array(matrix.length).fill(0);
+          for(let i=0; i<matrix.length; i++) {
+            if(matrix[i][j] != 0) {
+              if (arr[pointer]==0) {
+                arr[pointer] = matrix[i][j];
+              }
+              else if(matrix[i][j] == arr[pointer] && i!=pointer) {
+                arr[pointer]*=2;
+                matrix[i][j]=0;
+                pointer++;
+              }
+              else if(matrix[i][j] != arr[pointer] && arr[pointer] !=0) {
+                  arr[pointer+1] = matrix[i][j];
+                  matrix[i][j] = 0;
+                  pointer++;
+              }
+            }
+          }
+          for(let i=0;i<matrix.length; i++) {
+          if(copya[i][j] != arr[i]) played=true; 
+        }
+          for(let i=0; i<matrix.length; i++) {
+            matrix[i][j] = arr[i];
+          }
+        }
+        
+        break;
+    case "ArrowDown":
+      //j is the collum of the matrix
+      for(let j=0; j<matrix[0].length; j++) {
+        let pointer = matrix[0].length-1;
+        let arr = Array(matrix.length).fill(0);
+        for(let i=matrix[0].length-1; i>=0; i--) {
+          if(matrix[i][j] != 0) {
+            if (arr[pointer]==0) {
+              arr[pointer] = matrix[i][j];
+            }
+            else if(matrix[i][j] == arr[pointer] && i!=pointer) {
+              arr[pointer]*=2;
+              matrix[i][j]=0;
+              pointer--;
+            }
+            else if(matrix[i][j] != arr[pointer] && arr[pointer] !=0) {
+                arr[pointer-1] = matrix[i][j];
+                matrix[i][j] = 0;
+                pointer--;
+            }
+          }
+        }
+        for(let i=0;i<matrix.length; i++) {
+        if(copya[i][j] != arr[i]) played=true; 
+      }
+        for(let i=0; i<matrix.length; i++) {
+          matrix[i][j] = arr[i];
+        }
+      }
+      
+      break;
+    case "ArrowRight":
+      for (let i=0; i<matrix.length; i++) {
+        let pointer = matrix[i].length-1;
+        let arr = Array(matrix[i].length).fill(0);
+        for(let j=matrix[i].length-1; j>=0; j--) {
+          if(matrix[i][j] != 0) {
+            if (arr[pointer]==0) {
+              arr[pointer] = matrix[i][j];
+            }
+            else if(matrix[i][j] == arr[pointer] && j!=pointer) {
+              arr[pointer]*=2;
+              matrix[i][j]=0;
+              pointer--;
+            }
+            else if(matrix[i][j] != arr[pointer] && arr[pointer] !=0) {
+                arr[pointer-1] = matrix[i][j];
+                matrix[i][j] = 0;
+                pointer--;
+            }
+          }
+        }
+        for(let j=0;j<matrix[i].length; j++) {
+          if(copya[i][j] != arr[j]) played=true; 
+        }
+        matrix[i]=arr;
+      }
     default:
-      console.log("bbb");
+      return [matrix, played];
   }
-  return matrix;
+  return [matrix, played];
 }
 
 function SquaresBoard() {
   
 }
-// You pressed a key. ArrowLeft
-// App.js:34 You pressed a key. ArrowUp
-// App.js:34 You pressed a key. ArrowRight
-// App.js:34 You pressed a key. ArrowDown
+function IsNew(newsquare, square) {
+  return (newsquare[0]==square[0] && newsquare[1]==square[1])
+}
 
+
+var first = true;
 function Board () {
   function handleKeyPress(e) {
-    console.log( "You pressed a key.",e.code)
-    let merged = mergesquares(e, matrix);
+    var probability = function(n) {
+      return !!n && Math.random() <= n;
+    };
+    let played = false;
+    let data = mergesquares(e, matrix);
+    let merged = data[0];
+    played = data[1];
     setMatrix(merged);
+    if(first==true||played==true) {
     const array = generaterandomnumber(matrix);
+    
     let copya = [...matrix];
+    if(probability(0.8)) {
     copya[array[0]][array[1]] = 2;
+    }
+    else {
+      copya[array[0]][array[1]] = 4;
+    }
     setMatrix(copya);
-    //console.log(matrix[array[0]][array[1]])
+    setNewsquare(array)
+    first = false;
+    }
   }
+  const [newsquare, setNewsquare] = useState([0,0]);
   const [matrix, setMatrix] = useState(Array(4).fill().map(()=> Array(4).fill(0)));
-  console.log(matrix[0]);
   return  (
     <div className="board-container">
       <div className="board" onKeyDown={(e) => handleKeyPress(e)} tabIndex={-1}>
       <input type="hidden" onKeyPress={(e) => handleKeyPress(e)} />
       <div className="board-row">
-        <Square num = {matrix[0][0]} />
-        <Square num = {matrix[0][1]} />
-        <Square num = {matrix[0][2]} />
-        <Square num = {matrix[0][3]} />
+        <Square num = {matrix[0][0]} new = {IsNew(newsquare, [0,0])} />
+        <Square num = {matrix[0][1]} new = {IsNew(newsquare, [0,1])}/>
+        <Square num = {matrix[0][2]} new = {IsNew(newsquare, [0,2])}/>
+        <Square num = {matrix[0][3]} new = {IsNew(newsquare, [0,3])}/>
       </div>
       <div className="board-row">
-        <Square num = {matrix[1][0]} />
-        <Square num = {matrix[1][1]} />
-        <Square num = {matrix[1][2]} />
-        <Square num = {matrix[1][3]} />
+        <Square num = {matrix[1][0]} new = {IsNew(newsquare, [0,0])}/>
+        <Square num = {matrix[1][1]} new = {IsNew(newsquare, [1,1])}/>
+        <Square num = {matrix[1][2]} new = {IsNew(newsquare, [1,2])}/>
+        <Square num = {matrix[1][3]} new = {IsNew(newsquare, [1,3])}/>
       </div>
       <div className="board-row">
-        <Square num = {matrix[2][0]} />
-        <Square num = {matrix[2][1]} />
-        <Square num = {matrix[2][2]} />
-        <Square num = {matrix[2][3]} />
+        <Square num = {matrix[2][0]} new = {IsNew(newsquare, [2,0])}/>
+        <Square num = {matrix[2][1]} new = {IsNew(newsquare, [2,1])}/>
+        <Square num = {matrix[2][2]} new = {IsNew(newsquare, [2,2])}/>
+        <Square num = {matrix[2][3]} new = {IsNew(newsquare, [2,3])}/>
       </div>
       <div className="board-row">
-        <Square num = {matrix[3][0]} />
-        <Square num = {matrix[3][1]} />
-        <Square num = {matrix[3][2]} />
-        <Square num = {matrix[3][3]} />
+        <Square num = {matrix[3][0]} new = {IsNew(newsquare, [3,0])}/>
+        <Square num = {matrix[3][1]} new = {IsNew(newsquare, [3,1])}/>
+        <Square num = {matrix[3][2]} new = {IsNew(newsquare, [3,2])}/>
+        <Square num = {matrix[3][3]} new = {IsNew(newsquare, [3,3])}/>
       </div>
       </div>
     </div>
@@ -127,7 +215,7 @@ function Board () {
 
 function Square(props) {
   let className = "square";
-  className += " " + "square" + String(props.num) + "color";
+  className += " " + "square" + String(props.num) + "color" + " " + (props.new ? "new" : "old");
   return (
     <div className={className}>{props.num}</div>
   
